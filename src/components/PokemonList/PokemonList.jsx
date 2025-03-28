ba7086575cad35e8984aa05d8d1322c3314d3471
Daniel Scott <daniel@mydeveloperjourney.com>
2025-03-28 15:00:29 -0500

    wip: added dynamic link to list page component

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,11 +1,15 @@
+import { Link } from 'react-router';
+
 const PokemonList = (props) => {
     return (
         <>
             <h2>Pokemon</h2>
             <ul>
                 {props.pokemon.map((currentPokemon) => (
-                    <li key={currentPokemon.name}>
-                        {currentPokemon.name}
+                    <li key={currentPokemon._id}>
+                        <Link to={`/pokemon/${currentPokemon._id}`}>
+                            {currentPokemon.name}
+                        </Link>
                     </li>
                 ))}
             </ul>
