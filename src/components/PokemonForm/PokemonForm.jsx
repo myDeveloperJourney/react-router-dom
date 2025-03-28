febee05a03e15c8307890146abbabd084071907f
Daniel Scott <daniel@mydeveloperjourney.com>
2025-03-28 15:00:14 -0500

    wip: build initial new page page component

---
+++ src/components/PokemonForm/PokemonForm.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+
+const initialState = {
+    name: '',
+    weight: 0,
+    height: 0,
+};
+
+const PokemonForm = (props) => {
+    const [formData, setFormData] = useState(initialState);
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        // TODO: add submit logic here
+    };
+    const handleChange = (evt) => {
+        // update form state while user types into the form fields
+    };
+
+    return (
+        <main>
+            <h2>New Pokemon</h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Name:</label>
+                <input 
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange} 
+                />
+                <label htmlFor="weight">Weight:</label>
+                <input 
+                    type="text"
+                    id="weight"
+                    name="weight"
+                    value={formData.weight}
+                    onChange={handleChange} 
+                />
+                <label htmlFor="height">Height:</label>
+                <input 
+                    type="text"
+                    id="height"
+                    name="height"
+                    value={formData.height}
+                    onChange={handleChange} 
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </main>
+    );
+};
+
+export default PokemonForm;
